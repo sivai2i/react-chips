792fdc5457b2e2b923607949f1cb621ed3e10a39
sivai2i <sivakumar@ideas2it.com>
2018-05-02 11:17:17 +0530

    Changed lib version

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom'
-import Chips from 'react-chips'
-
-let items = ["Java", "Ruby", "Javascript", "C", "C++"]
-
-render(<Chips autoCompleteData={items}/>, document.getElementById("root"));
